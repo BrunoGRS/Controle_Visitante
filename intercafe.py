9d7efd42346c4d599379b73fff0dd3dd0a5f4dd1
Bruno <brunorodriguesr01@gmail.com>
2023-09-15 17:12:06 -0300

    Controle v4

--- intercafe.py
+++ intercafe.py
@@ -1,23 +1,38 @@
 import flet as ft
+from banco import Conexao
 
-def main(page):
-
-    first_name = ft.TextField(label="First name", autofocus=True)
-    last_name = ft.TextField(label="Last name")
+def main(page: ft.Page):
+    page.horizontal_alignment = 'center'
+    page.vertical_alignment = 'center'
+    page.window_height = 500
+    page.window_width = 500
     
+    titulo = ft.Text(value="Cadastro de visitantes", font_family="Cascadia Mono", size=20)
 
-    def btn_click(e):
-        greetings.controls.append(ft.Text(f"Hello, {first_name.value} {last_name.value}!"))
-        first_name.value = ""
-        last_name.value = ""
-        page.update()
-        first_name.focus()
+    user = ft.TextField(label="Usuário", border_color="white", width=350)
 
-    page.add(
-        first_name,
-        last_name,
-        ft.ElevatedButton("Say hello!", on_click=btn_click),
-        greetings,
-    )
+    senha = ft.TextField(label="Senha", border_color="white", width=350)
+
+    def button_clicked(e):
+        if not user.value or len(str(user)) > 15:
+            user.error_text="Campo Iválido."
+            page.update()
+        if not senha.value:
+            senha.error_text="Informe a senha!"
+            page.update()
+        else:
+            entrada1 = user.value
+            entrada2 = senha.value
+
+            validar = Conexao.validar_usuario(Conexao, entrada1, entrada2)
+            if validar == "Validado":
+                page.clean()
+                titulo = ft.Text(value="Cadastro de visitantes", font_family="Cascadia Mono", size=20)
+                page.add(titulo)
+            else:
+                warning = page.error("Acesso negado")
+                warning.remove()
+
+    page.add(titulo, user, senha, ft.FilledButton(text="Entrar", on_click=button_clicked))
 
 ft.app(target=main)
